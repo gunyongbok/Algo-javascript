a922a5d9273b34bcbc4b7282eb7787d66e9970b3
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-14 14:12:19 +0900

    Day 1

---
+++ LV_0/Day1.js
@@ -0,0 +1,19 @@
+function solution(num1, num2) {
+    let answer = num1 + num2;
+    return answer;
+}
+
+function solution1(num1, num2) {
+    var answer = num1 - num2;
+    return answer;
+}
+
+function solution2(num1, num2) {
+    var answer = num1 * num2;
+    return answer;
+}
+
+function solution3(num1, num2) {
+    var answer = num1 / num2;
+    return Math.floor(answer);
+}
