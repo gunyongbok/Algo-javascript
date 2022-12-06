1b18f1721f201ac35af2ac2c2d8f0ac18908b9c9
gunyongbok <qhrrjsdyd123@gmail.com>
2022-12-06 23:07:36 +0900

    Day 22

--- LV_0/Day22.js
+++ LV_0/Day22.js
@@ -22,11 +22,41 @@ function solution3(before, after) {
     return 0;
 }
 
-function solution(i, j, k) {
+function solution4(i, j, k) {
     let container = [];
     for (let start = i; start <= j; start++) {
         container.push(String(start).split(''));
     }
     let count = container.flat(Infinity).filter((element) => `${k}` === element).length;
     return count;
+}
+
+function solution5(A, B) {
+    let arr = [...A];
+    for (let i = 0; i < arr.length; i++) {
+        if (A === B) return i;
+        else {
+            arr.unshift(arr.pop());
+            if (arr.join('') === B) return i + 1;
+        }
+    }
+    return -1;
+}
+
+function solution6(M, N) {
+    return M - 1 + (N - 1) * M;
+}
+
+function solution7(num, total) {
+    const min = Math.ceil(total / num - Math.floor(num / 2));
+    return Array.from({ length: num }, (_, i) => i + min);
+}
+
+function solution(common) {
+    if (common[2] - common[1] === common[1] - common[0]) {
+        return common[common.length - 1] + common[2] - common[1];
+    }
+    if (common[2] / common[1] === common[1] / common[0]) {
+        return (common[common.length - 1] * common[1]) / common[0];
+    }
 }
