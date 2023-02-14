e8822d5f6cee18339703e1af6676fd15a0eae5aa
gunyongbok <qhrrjsdyd123@gmail.com>
2023-02-14 21:18:37 +0900

    Lv 2_Day 1

---
+++ LV_2/Day1.js
@@ -0,0 +1,22 @@
+function solution1(s) {
+    const arr = s.split(' ');
+
+    return Math.min(...arr) + ' ' + Math.max(...arr);
+}
+
+function solution(s) {
+    const arr = s.toLowerCase().split(' ');
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const first = arr[i].charAt(0);
+        if (isNaN(first) === false) {
+            newArr.push(arr[i]);
+        } else {
+            newArr.push(arr[i].replace(first, first.toUpperCase()));
+        }
+    }
+    return newArr.join(' ');
+}
+
+solution('3people unFollowed me');
