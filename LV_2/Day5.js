eb99c2c525d4ea4c60b636ea6d54b4a24dc77e0f
gunyongbok <qhrrjsdyd123@gmail.com>
2023-03-27 23:16:47 +0900

    Lv 2_Day 5

---
+++ LV_2/Day5.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (stack[stack.length - 1] != s[i]) {
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+        }
+    }
+
+    if (stack.length === 0) {
+        return 1;
+    }
+    return 0;
+}
